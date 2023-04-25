7bb0c1669714b7db6395772379b2269f41b21cd1
Rbttt <golomazdinp@mail.ru>
2023-04-25 17:13:19 +0300

    main page changes

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,10 +5,33 @@ import "./../../styles/Main.scss";
 export const Main = () => {
     const [statusForm] = useContext(StatusFormContext);
 
-    return (
+    return statusForm ? (
         <div className="page-container">
             <h1 className="page-container_heading">
                 <span>Задравствуйте,</span> {statusForm?.name ?? "пользователь"}
+            </h1>
+            <ul className="status-info">
+                <li className="status-info_item">
+                    <span>Имя</span> : {statusForm.name}
+                </li>
+                <li className="status-info_item">
+                    <span>Фамилия</span> : {statusForm.lastname}
+                </li>
+                <li className="status-info_item">
+                    <span>Город</span> : {statusForm.city}
+                </li>
+                <li className="status-info_item">
+                    <span>Телефон</span> : {statusForm.phone}
+                </li>
+                <li className="status-info_item">
+                    <span>Email</span> : {statusForm.email}
+                </li>
+            </ul>
+        </div>
+    ) : (
+        <div className="page-container">
+            <h1 className="page-container_heading">
+                <span>Задравствуйте,</span> пользователь
             </h1>
         </div>
     );
